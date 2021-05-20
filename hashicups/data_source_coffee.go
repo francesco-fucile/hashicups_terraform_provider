4c69e85f80c09229399898641bf166741bba995f
FrancescoFucile <francesc.fucile@ocyan.com>
2021-05-20 20:31:56 +0100

    implement READ.

--- hashicups/data_source_coffee.go
+++ hashicups/data_source_coffee.go
@@ -31,3 +31,39 @@ func dataSourceCoffees() *schema.Resource {
     },
   }
 }
+
+// read
+func dataSourceCoffeesRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+  client := &http.Client{Timeout: 10 * time.Second}
+
+  // Warning or errors can be collected in a slice type
+  var diags diag.Diagnostics
+
+  req, err := http.NewRequest("GET", fmt.Sprintf("%s/coffees", "http://localhost:19090"), nil)
+  if err != nil {
+    return diag.FromErr(err)
+  }
+
+  r, err := client.Do(req)
+  if err != nil {
+    return diag.FromErr(err)
+  }
+  defer r.Body.Close()
+
+  coffees := make([]map[string]interface{}, 0)
+  err = json.NewDecoder(r.Body).Decode(&coffees)
+  if err != nil {
+    return diag.FromErr(err)
+  }
+
+  if err := d.Set("coffees", coffees); err != nil {
+    return diag.FromErr(err)
+  }
+
+  // always run
+  d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+
+  return diags
+}
+
+  
