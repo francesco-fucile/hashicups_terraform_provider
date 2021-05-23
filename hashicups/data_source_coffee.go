7a1e112447c318a256f27e465bd2d6b210674921
FrancescoFucile <francesc.fucile@ocyan.com>
2021-05-23 15:38:57 +0100

    first chapter.

--- hashicups/data_source_coffee.go
+++ hashicups/data_source_coffee.go
@@ -16,19 +16,51 @@ func dataSourceCoffees() *schema.Resource {
   return &schema.Resource{
     ReadContext: dataSourceCoffeesRead,
     Schema: map[string]*schema.Schema{
-      Type: schema.TypeList,
-      Computed: true,
-      Elem: &schema.Resource{
-        Schema: map[string]*schema.Schema{
-          "id": &schema.Schema{
-            Type: schema.TypeInt,
-            Computed: true,
+      "coffees": &schema.Schema{
+        Type:     schema.TypeList,
+        Computed: true,
+        Elem: &schema.Resource{
+          Schema: map[string]*schema.Schema{
+            "id": &schema.Schema{
+              Type:     schema.TypeInt,
+              Computed: true,
+            },
+            "name": &schema.Schema{
+              Type:     schema.TypeString,
+              Computed: true,
+            },
+            "teaser": &schema.Schema{
+              Type:     schema.TypeString,
+              Computed: true,
+            },
+            "description": &schema.Schema{
+              Type:     schema.TypeString,
+              Computed: true,
+            },
+            "price": &schema.Schema{
+              Type:     schema.TypeInt,
+              Computed: true,
+            },
+            "image": &schema.Schema{
+              Type:     schema.TypeString,
+              Computed: true,
+            },
+            "ingredients": &schema.Schema{
+              Type:     schema.TypeList,
+              Computed: true,
+              Elem: &schema.Resource{
+                Schema: map[string]*schema.Schema{
+                  "ingredient_id": &schema.Schema{
+                    Type:     schema.TypeInt,
+                    Computed: true,
+                  },
+                },
+              },
+            },
           },
-          "name": &schema.Schema{
-            Type: schema.TypeString,
-            Computed: true,
-          },
-    },
+        },
+      },
+    },    
   }
 }
 
